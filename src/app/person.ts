a9a46a8cb3bbb6eff35c14345a9a042a78c80c9c
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-12-18 22:37:16 +0700

    Create 'Person' class

---
+++ src/app/person.ts
@@ -0,0 +1,11 @@
+export class Person {
+  id: number = 0;
+  firstName: string = '';
+  lastName: string = '';
+  
+  constructor() {
+    this.id = 0;
+    this.firstName = '';
+    this.lastName = '';
+  }
+}
