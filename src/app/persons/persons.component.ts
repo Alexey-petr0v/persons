91e5af79b0fa7464fcf7d80031776c5562c69546
Alexey-petr0v <Alexey-petr0v@yandex.ru>
2020-12-18 22:42:13 +0700

    Changed 'persons' component: import 'HttpClient' and 'Person' class; created synchronization and output of results to the page

--- src/app/persons/persons.component.ts
+++ src/app/persons/persons.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+
+import { Person } from '../person';
 
 @Component({
   selector: 'app-persons',
@@ -7,8 +10,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PersonsComponent implements OnInit {
 
-  constructor() { }
+  peoples = new Array();
+  x = '';
 
+  constructor(http:HttpClient) {
+
+    function getJSON(that: PersonsComponent) {
+      http.get('http://localhost:3000/persons').subscribe((response : any) => {
+        that.peoples = response.map(function(obj : Person) {
+          let person = new Person();
+          person.id = obj.id;
+          person.firstName = obj.firstName;
+          person.lastName = obj.lastName;
+          return person;
+        });
+      })
+    }
+
+    function setTimer(thatObj: Object, func: Function) {
+      setInterval((thatObj: any[]) => {return func(thatObj)}, 2000);
+    }
+    
+    getJSON(this);
+
+    let that = this;
+    setTimer(that, function() {
+      console.log(that);
+      getJSON(that);
+    })
+  }
+
+
+    // console.log("this.peoples:"+this.peoples)
+    // console.log("this.peoples instanceof Array: "+(this.peoples instanceof Array));
+    // console.log("this.peoples[0] instanceof Person: "+(this.peoples[0] instanceof Person));
   ngOnInit(): void {
   }
 
